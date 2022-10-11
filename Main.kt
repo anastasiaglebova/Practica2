88d797cc5887d0e4b06532b633a5a3b1c1d40a64
anastasiaglebova <99547855+anastasiaglebova@users.noreply.github.com>
2022-10-11 11:28:28 +0300

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -9,6 +9,7 @@ fun main(args: Array<String>) {
 
     //zd 3
     val avg1 = (age1.toDouble() + age2.toDouble()) / 2
+    println(avg1) 
     //После того, как добавили toDouble, мы получили нужный тип
 
     //zd 4
@@ -17,12 +18,15 @@ fun main(args: Array<String>) {
 
     //zd 5
     val fullName = firstName + " " + lastName
+    println(fullName) 
 
     //zd 6
     val myDetails = "Привет, меня зовут $fullName"
+    println(myDetails) 
 
     // zd 7
-    val date: Triple <Int, Int, Int> = Triple(first:10, second: 7, third: 2002)
+    val date: Triple <Int, Int, Int> = Triple(10, 7, 2002)
+    println(date) 
 
     //zd 8
     val month = date.first
@@ -30,9 +34,10 @@ fun main(args: Array<String>) {
     val year = date.third
 
     //zd 9
-    var (month, _, year) = date
+    var (month1, _, year1) = date
 
     //zd 10
-
-
+    month = 5
+    val dateUpdate = Pair(month1, year1) 
+    println(dateUpdate) 
 }
